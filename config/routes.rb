bdc13751e7e1939381bf49d4a2ddd6845d6386e7
Thomas Bierlaire <thomas.bierlaire@gmail.com>
2017-03-28 19:30:51 +0200

    Ajout controller, model et view/index pour les 4 tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/team" => "pages#team"
+  get "/contact" => "pages#contact"
+
+  get "/users" => "users#index"
+  get "/articles" => "articles#index"
+  get "/lists" => "lists#index"
+  get "/shops" => "shops#index"
+
 end
