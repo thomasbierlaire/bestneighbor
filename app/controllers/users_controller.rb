3b381c1589565bcc9242d5a7b3d7bf0b34e85603
Thomas Bierlaire <thomas.bierlaire@gmail.com>
2017-04-01 15:17:36 +0200

    ajout formulaire création user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to users_path
+  end
+
+  private
+  # Cette fonction permet de protéger le formulaire
+  # Seules les données permises seront sauvegardées en base
+  def user_params
+    params.require(:user).permit(:name, :email, :passwd)
+  end
+
 end
