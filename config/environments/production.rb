b125d914cd71112a1a16ac5652c9c92ee5bc6ba7
Thomas Bierlaire <thomas.bierlaire@gmail.com>
2017-04-21 11:56:45 +0200

    maj pour test mail via mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,25 +85,26 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-#config.action_mailer.delivery_method = :smtp
-# SMTP settings for mailgun
-#ActionMailer::Base.smtp_settings = {
-#  :port           => "587",
-#  :address        => "smtp.mailgun.org",
-#  :user_name      => ENV['username'],
-#  :password       => ENV['password'],
-#  :authentication => :plain,
-#}
-
 config.action_mailer.delivery_method = :smtp
-# SMTP settings for gmail
-config.action_mailer.smtp_settings = {
- :address              => "smtp.bestneighbor.fr",
- :port                 => 587,
- :user_name            => ENV['gmail_username'],
- :password             => ENV['gmail_password'],
- :authentication       => :plain,
- :openssl_verify_mode  => 'none'
+# SMTP settings for mailgun
+ActionMailer::Base.smtp_settings = {
+  :port           => "587",
+  :address        => "smtp.mailgun.org",
+  :domain         => ENV['domain'],
+  :user_name      => ENV['username'],
+  :password       => ENV['password'],
+  :authentication => :plain,
 }
+
+#config.action_mailer.delivery_method = :smtp
+# SMTP settings for gmail
+#config.action_mailer.smtp_settings = {
+# :address              => "smtp.bestneighbor.fr",
+# :port                 => 587,
+# :user_name            => ENV['gmail_username'],
+# :password             => ENV['gmail_password'],
+# :authentication       => :plain,
+# :openssl_verify_mode  => 'none'
+#}
 
 end
