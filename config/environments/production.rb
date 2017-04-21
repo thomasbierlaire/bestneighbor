66dad96c03554402f2a601e28de1e2075a668eb1
Thomas Bierlaire <thomas.bierlaire@gmail.com>
2017-04-21 15:08:35 +0200

    maj pour test mail via mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,8 +98,9 @@ Rails.application.configure do
 config.action_mailer.delivery_method = :smtp
 # SMTP settings for mailgun
 ActionMailer::Base.smtp_settings = {
-  :port           => "587",
+  :port           => "465",
   :address        => "smtp.mailgun.org",
+  :domain         => ENV['domain'],
   :user_name      => ENV['username'],
   :password       => ENV['password'],
   :authentication => :plain,
