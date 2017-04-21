de5255780815ad4e6609bb369c08c2258fa0ba7b
Thomas Bierlaire <thomas.bierlaire@gmail.com>
2017-04-21 11:40:22 +0200

    maj pour test mail via gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,12 +98,12 @@ Rails.application.configure do
 config.action_mailer.delivery_method = :smtp
 # SMTP settings for gmail
 config.action_mailer.smtp_settings = {
- :address              => "smtp.gmail.com",
+ :address              => "smtp.breadme.fr",
  :port                 => 587,
  :user_name            => ENV['gmail_username'],
  :password             => ENV['gmail_password'],
  :authentication       => :plain,
- :openssl_verify_mode  => 'none'
+ :enable_starttls_auto => true
 }
 
 end
