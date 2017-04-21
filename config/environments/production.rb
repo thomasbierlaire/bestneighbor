265e98fca41a1d8bd5029ff9fa884e953af3b4a4
Thomas Bierlaire <thomas.bierlaire@gmail.com>
2017-04-21 14:16:44 +0200

    maj pour test mail via gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -107,12 +107,12 @@ Rails.application.configure do
 
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
+  address:              "smtp.gmail.com",
   port:                 587,
-  domain:               'bestneighbor.fr',
+  domain:               "bestneighbor.fr",
   user_name:            ENV['gmail_username'],
   password:             ENV['gmail_password'],
-  authentication:       'plain',
+  authentication:       "plain",
   enable_starttls_auto: true  }
 
 end
