a39aca0d28b49143f6da93cd243cc368fe5b850d
Thomas Bierlaire <thomas.bierlaire@gmail.com>
2017-04-21 12:09:07 +0200

    maj pour test mail via mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-
+config.action_mailer.default_url_options = { host: 'bestneighbor.herokuapp.com' }
 config.action_mailer.delivery_method = :smtp
 # SMTP settings for mailgun
 ActionMailer::Base.smtp_settings = {
