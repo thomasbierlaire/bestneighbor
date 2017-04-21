149108bae742f3d1935aa9dee364cf0f7610f7c8
Thomas Bierlaire <thomas.bierlaire@gmail.com>
2017-04-21 11:26:15 +0200

    maj pour test mail via gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,7 @@ config.action_mailer.delivery_method = :smtp
 # SMTP settings for gmail
 config.action_mailer.smtp_settings = {
  :address              => "smtp.gmail.com",
- :port                 => "587",
+ :port                 => 587,
  :domain               => "gmail.com",
  :user_name            => ENV['gmail_username'],
  :password             => ENV['gmail_password'],
