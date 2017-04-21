4900da059ebf74f043c51ed0d7faa39512889a27
Thomas Bierlaire <thomas.bierlaire@gmail.com>
2017-04-21 14:46:48 +0200

    maj pour test mail via gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,27 +84,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-#config.action_mailer.delivery_method = :smtp
-# SMTP settings for mailgun
-#ActionMailer::Base.smtp_settings = {
-#  :port           => 587,
-#  :address        => "smtp.mailgun.org",
-#  :domain         => ENV['domain'],
-#  :user_name      => ENV['username'],
-#  :password       => ENV['password'],
-#  :authentication => :plain,
-#}
-
-#ActionMailer::Base.smtp_settings = {
-#  :port           => ENV['MAILGUN_SMTP_PORT'],
-#  :address        => ENV['MAILGUN_SMTP_SERVER'],
-#  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-#  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-#  :domain         => 'bestneighbor.herokuapp.com',
-#  :authentication => :plain,
-#}
-#ActionMailer::Base.delivery_method = :smtp
-
 config.action_mailer.delivery_method = :smtp
 # SMTP settings for gmail
 config.action_mailer.smtp_settings = {
@@ -115,3 +94,5 @@ config.action_mailer.smtp_settings = {
  :authentication       => "plain",
 :enable_starttls_auto => true
 }
+
+end
