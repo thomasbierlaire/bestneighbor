3b21017ab7b89bde44b3e3cdfd4a2644075d9039
Thomas Bierlaire <thomas.bierlaire@gmail.com>
2017-04-21 14:41:01 +0200

    maj pour test mail via gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,15 +104,14 @@ Rails.application.configure do
 #  :authentication => :plain,
 #}
 #ActionMailer::Base.delivery_method = :smtp
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  address:              "smtp.gmail.com",
-  port:                 587,
-  domain:               "smtp.gmail.com",
-  user_name:            ENV["gmail_username"],
-  password:             ENV["gmail_password"],
-  authentication:       "login",
-  enable_starttls_auto: true  }
 
-end
+config.action_mailer.delivery_method = :smtp
+# SMTP settings for gmail
+config.action_mailer.smtp_settings = {
+ :address              => "smtp.gmail.com",
+ :port                 => 587,
+ :user_name            => ENV['gmail_username'],
+ :password             => ENV['gmail_password'],
+ :authentication       => "plain",
+:enable_starttls_auto => true
+}
