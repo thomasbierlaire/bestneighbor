89d764bae76eea2d614b536e4f6741f428a044f7
Thomas Bierlaire <thomas.bierlaire@gmail.com>
2017-04-30 12:18:00 +0200

    mise à jour meta title et description pour toutes les pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def title(title_content)
+    if title_content.present? # si le paramètre title_content est présent
+      title_content << " | Bestneighbor" # le title reprend la chaîne en paramètre suivie de " | Mon Site"
+    else
+      "Bestneighbor" # sinon, par défaut, le title sera "Mon site"
+    end
+  end
+
+  def description(description_content)
+    if description_content.present?
+      description_content << " | Bestneighbor"
+    else
+      "Bestneighbor - Services entre voisins - courses et covoiturage" # sinon, par défaut, le title sera "Mon site"
+    end
+  end
+
 end
--- app/helpers/liste_fournisseurs_helper.rb
+++
@@ -1,2 +0,0 @@
-module ListeFournisseursHelper
-end
