e3d53c813ffd3f7b702f76213ca00e14b318b8f4
Thomas Bierlaire <thomas.bierlaire@gmail.com>
2017-06-13 20:51:12 +0200

    mise en place module devise confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
   has_many :trajets
 
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :recoverable:timeoutable and :omniauthable
 
-  # ajouter :registerable pour possibilité de création de user
   # ajouter :recoverable pour possibilité de mot de passe oublié
 
   devise :database_authenticatable, :rememberable,
-         :trackable, :validatable, :registerable, :lockable
+         :trackable, :validatable, :registerable, :lockable,
+         :confirmable
 end
