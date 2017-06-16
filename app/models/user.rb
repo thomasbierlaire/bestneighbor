19d78cfc08709f8e66463bd40012ff4fe7471119
Thomas Bierlaire <thomas.bierlaire@gmail.com>
2017-06-16 16:42:53 +0200

    suppression option rememberable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,10 @@ class User < ApplicationRecord
   has_many :trajets, dependent: :destroy
 
   # Include default devise modules. Others available are:
-  # :recoverable:timeoutable and :omniauthable
+  # :rememberable, :recoverable, :timeoutable and :omniauthable
 
   # ajouter :recoverable pour possibilité de mot de passe oublié
 
-  devise :database_authenticatable, :rememberable,
-         :trackable, :validatable, :registerable, :lockable,
+  devise :database_authenticatable, :trackable, :validatable, :registerable, :lockable,
          :confirmable
 end
