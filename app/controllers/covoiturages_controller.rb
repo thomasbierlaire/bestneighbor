9d4d13e058051b12503d32073bcbea583baa8a73
Thomas Bierlaire <thomas.bierlaire@gmail.com>
2017-11-17 18:21:30 +0100

    test envoi sms

--- app/controllers/covoiturages_controller.rb
+++ app/controllers/covoiturages_controller.rb
@@ -97,32 +97,11 @@ class CovoituragesController < ApplicationController
       UserMailer.take_covoiturage_email(current_user, @cov, @user, @cas).deliver_later
 
 #########################################################
+# TEST
+      @phonenumber = "+33630809785"
+      @message = "Votre proposition de trajet du #{@cov.date.to_date.strftime("%d %b %Y")} pour #{@cov.destination} est réservée par #{current_user.email}"
+      send_sms(@phonenumber, @message)
 
-#      @phonenumber = "+33630809785"
-
-#      @message = "Votre proposition de trajet du #{@cov.date.to_date.strftime("%d %b %Y")} pour #{@cov.destination} est réservée par #{current_user.email}"
-
-#      uri = URI.parse("https://api.smspartner.fr/v1/send")
-#      request = Net::HTTP::Post.new(uri)
-#      request.content_type = "application/json"
-#      request.body = JSON.dump({
-#        "apiKey" => "7059942ad5244ef71e98d0107f21d1153f9946bf",
-#        "phoneNumbers" => "#{@phonenumber}",
-#        "message" => "#{@message}",
-#        "sender" => "BNeighbor",
-#        "gamme" => "2"
-#      })
-
-#      req_options = {
-#        use_ssl: uri.scheme == "https",
-#      }
-
-#      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-#        http.request(request)
-#      end
-
-#      puts "RESPONSE CODE" + response.code
-#      puts "RESPONSE BODY" + response.body
 ############################################################
 
       redirect_to covoit_path
@@ -209,6 +188,30 @@ class CovoituragesController < ApplicationController
 
   def find_covoiturage
     @cov = Covoiturage.find(params[:id])
+  end
+
+  def send_sms(phonenumber, message)
+    uri = URI.parse("https://api.smspartner.fr/v1/send")
+    request = Net::HTTP::Post.new(uri)
+    request.content_type = "application/json"
+    request.body = JSON.dump({
+      "apiKey" => "7059942ad5244ef71e98d0107f21d1153f9946bf",
+      "phoneNumbers" => "#{phonenumber}",
+      "message" => "#{message}",
+      "sender" => "BNeighbor",
+      "gamme" => "2"
+    })
+
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+
+    puts "RESPONSE CODE" + response.code
+    puts "RESPONSE BODY" + response.body
   end
 
 end
